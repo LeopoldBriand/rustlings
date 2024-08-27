85a10de0d5ae6ace87f69191d7d65f3f73512c8f
Leopold Briand <53423281+LeopoldBriand@users.noreply.github.com>
2024-08-27 08:34:37 +0000

    fix rustling install

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,5 +3,5 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Update current shell environment variables after install to find rustup
 . "$HOME/.cargo/env"
-rustup install stable
-bash install.sh
+
+cargo install rustlings
